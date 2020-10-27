3059baf709a83a72c56beffff335b46dade1607f
jhuang1085 <huangjingyi85@qq.com>
2020-10-26 23:37:11 -0500

    mygames

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { GamedisplayComponent } from './gamedisplay/gamedisplay.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GamesComponent} from './games/games.component';
+import {MygamesComponent} from './mygames/mygames.component';
 
 const routes: Routes = [
   {path: 'games', component: GamesComponent, children: [{
     path: 'games?index=type', component: GamesComponent
   }]},
   {path: 'home', component: HomeComponent},
-  {path: 'userListing', component: ListsComponent}
+  {path: 'userListing', component: ListsComponent},
+  {path: 'myGames', component: MygamesComponent}
 ];
 
 @NgModule({
---
+++ src/app/mygames/types.ts
@@ -0,0 +1,22 @@
+export interface List {
+  title: string;
+  date: string;
+}
+
+export interface Item {
+  title: string;
+  id: string;
+  href: string;
+}
+
+export interface CustomList {
+  Title: string;
+  Description: string;
+  Score: string;
+  Type: string;
+}
+
+export interface Image {
+  index: string;
+  type: string;
+}
