b116140bf00be626f8f4e6bc1b14ecba96c35e44
John Diatte <jcdiatte@gmail.com>
2020-10-26 22:03:05 -0500

    gamedisplay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { GamesComponent } from './games/games.component';
+import { ListsComponent } from './lists/lists.component';
+import { MygamesComponent } from './mygames/mygames.component';
+import { GamedisplayComponent } from './gamedisplay/gamedisplay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'games', component: GamesComponent},
+  {path: 'userListing', component: ListsComponent},
+  {path: 'myGames', component: MygamesComponent},
+  {path: 'gameDisplay', component: GamedisplayComponent},
+  {path: 'userProfile', component: UserProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/gamedisplay/gamedisplay.component.ts
+++ src/app/gamedisplay/gamedisplay.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export class GameInfo {
+  public cover: string;
+  public title: string;
+  public bio: string;
+  public release_date: string;
+  public genre: string;
+  public type: string;
+  public developer: string;
+}
+
 @Component({
   selector: 'app-gamedisplay',
   templateUrl: './gamedisplay.component.html',
@@ -7,9 +17,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamedisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.game.bio = "Join your crewmates in a multiplayer game of teamwork and betrayal! Play online or over local wifi with 4-10 players as you attempt to hold your spaceship together and return back to civilization. But beware...as there may be an alien impostor aboard!";
+    this.game.cover = "https://cdn-products.eneba.com/resized-products/InlPQPt7WRDHBRnySANJHVQrS2whLdUdTX9JB7djmGw_350x200_3x-0.jpeg";
+    this.game.developer = "Innersloth";
+    this.game.genre = "Sci-fi";
+    this.game.release_date = "June 15, 2018";
+    this.game.title = "Among Us";
+    this.game.type = "Multiplace, Online Co-Op, Indie";
   }
+
+  game = new GameInfo();
+
+
 
 }
