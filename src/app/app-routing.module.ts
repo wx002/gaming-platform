9b885af69a83729b4399d4d78660f347c2765be5
Ben WeiJian Xu <wx002@bucknell.edu>
2020-10-26 20:56:21 -0500

    create skeleton code for games component with navbar, temp setup with dynamic url for index selection for searching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { ListsComponent } from './lists/lists.component';
+import { HomeComponent } from './home/home.component';
+import { GamedisplayComponent } from './gamedisplay/gamedisplay.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GamesComponent} from './games/games.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'games', component: GamesComponent, children: [{
+    path: 'games?index=type', component: GamesComponent
+  }]},
+  {path: 'home', component: HomeComponent},
+  {path: 'userListing', component: ListsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingArray = [GamesComponent];
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+  readonly alphaListing = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+
   }
 
 }
