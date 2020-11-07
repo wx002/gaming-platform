3891cfa4e38a59471a10f14de61e2324164a1dfc
Farhaana Mujid <farhaanamujid@farhaanas-mbp.attlocal.net>
2020-11-07 15:08:18 -0600

    lists component was re-added

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListsComponent implements OnInit {
 
+  user: string = 'amkasumi';
+  title: string = 'Games Like Animal Crossing';
+  description: string = '';
+
+  lists: any[]= [
+    { user: 'amkasumi', title: 'Games Like Animal Crossing', description: 'Chill games that involve mundane tasks. Can build things, dress up, raise pets, etc. Playtime = infinate.'},
+    { user: 'yogataichi34', title: 'Best Games For Switch Lite', description: 'Some games are just waaay better on the regular switch console but these are games where the experience on the Lite is still very comparable.'},
+    { user: 'rageofbahamut404', title: 'Fav Gacha Games', description: 'Mainly card-based'},
+    { user: 'iloveanimalsss01', title: 'Worst Indie Games of 2020', description: 'All genres'},
+];
+
   constructor() { }
 
   ngOnInit(): void {
